9f600cb03df4a1ace7674837c77501d2d273dbb3
Tiefgef4llen <Tiefgef4llen06@gmx.de>
2025-05-14 08:44:07 +0200

    t

--- INF2/BS/BS1/listing.c
+++ INF2/BS/BS1/listing.c
@@ -6,34 +6,32 @@
 typedef struct string_c {
     unsigned int size;
     char name[50];
-} string_t;
+} string_struct;
  
 /* Thread-Funktion */
 void *example_fct(void *args){
-    /* DieÜbergabe wird zurück aufeinen int-Pointer gecastet*/
+    /* DieÜbergabe wird zurück aufeinen struct-Pointer gecastet*/
     //uintptr_t *inParam=(uintptr_t*)args;
-    string_t* str = (string_t*)args;
+
+    printf("Thread Funktion...\n");
+    string_struct* str = (string_struct*)args;
     str->size = strlen(str->name);   
  
     return NULL;
 }
 
 int main(){
-    /* Legeein Thread-Handle, einenÜbergabe-und einen Rückgabeparameter an*/
     pthread_t thread;
 
-    string_t str = {0, "Julian"};
+    string_struct str = {0, "Julian"};
 
     printf("Length: %u,  Name: %s\n", str.size, str.name);
 
     pthread_create(&thread,NULL,&example_fct,(void*)&str);
+
     /* Warteauf Beendigung des Threads */
-    //pthread_join(thread,(void **)(&threadRetParam));
     pthread_join(thread, NULL);
 
-
-    /* Inhalt desRückgabeparameters ausgeben */
-    //printf("Rueckgabe von Thread:%llu\n",threadRetParam);
     printf("Length: %u,  Name: %s\n", str.size, str.name);
 
     return 0;
